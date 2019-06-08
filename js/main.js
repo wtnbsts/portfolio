8aee205b3801178e36fa9dec1566b07b5ff0c533
wtnbsts <watanabes1981@gmail.com>
2019-06-08 22:28:05 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function() {
 
 	const menu = $('.navigator');
+	const secondmenu = $('ul.menu_second');
+	const contents = $('#contents');
+	const body = $('html, body');
+	const submenu = $('li.sub_menu');
 
 	var pictBottom;
 	$(window).scroll(function() {
@@ -12,49 +16,49 @@ $(function() {
 		}
 	});
 
-	$('li.sub_menu').on({
+	submenu.on({
 		'mouseover' : function() {
-			$('ul.menu_second').stop().slideDown('fast');
+			secondmenu.stop().slideDown('fast');
 		},
 		'mouseout' : function() {
-			$('ul.menu_second').stop().slideUp('fast');
+			secondmenu.stop().slideUp('fast');
 		}
 	});
 
 	$('#nav_self').on({
 	    'click': function() {
-	        $('#contents').load("home.html", function(response, status, xhr) {
+	        contents.load("home.html", function(response, status, xhr) {
 			});
 	    }
 	});
 	$('#nav_skill').on({
 	    'click': function() {
-	        $('#contents').load("skill.html", function(response, status, xhr) {
-				$('html, body').scrollTop(0);
+	        contents.load("skill.html", function(response, status, xhr) {
+				body.scrollTop(0);
 	        });
 	    }
 	});
 	$('#project').on({
 	    'click': function() {
-			$('#contents').load("jisseki_project.html", function(response, status, xhr) {
-				$('html, body').scrollTop(0);
-				$('li.sub_menu').mouseout();
+			contents.load("jisseki_project.html", function(response, status, xhr) {
+				body.scrollTop(0);
+				submenu.mouseout();
 			});
 		}
 	});
 	$('#quality').on({
 	    'click': function() {
-			$('#contents').load("jisseki_quality.html", function(response, status, xhr) {
-				$('html, body').scrollTop(0);
-				$('li.sub_menu').mouseout();
+			contents.load("jisseki_quality.html", function(response, status, xhr) {
+				body.scrollTop(0);
+				submenu.mouseout();
 			});
 		}
 	});
 
 	$('#nav_vision').on({
 	    'click': function() {
-			$('#contents').load("vision.html", function(response, status, xhr) {
-				$('html, body').scrollTop(0);
+			contents.load("vision.html", function(response, status, xhr) {
+				body.scrollTop(0);
 			});
 		}
 	});
